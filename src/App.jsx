f3af7bd77b6edf9dfc22530f7beecf7975c387d6
Diego Fuenzalida <fuenzaladeveloper@gmail.com>
2023-12-11 12:39:57 -0300

    El verdadero first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <form onSubmit={e => e.preventDefault()}>
+        <button></button>
+      </form>
+    </>
+  )
+}
+
+export default App
